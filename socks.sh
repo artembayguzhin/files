8cf92ae3a5dca1902f182dbcf571d00a3e4cd12c
artembayguzhin <84047125+artembayguzhin@users.noreply.github.com>
2021-06-09 17:21:54 +0300

    Add files via upload

---
+++ socks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "Please enter your pass: "
+read pass
+
+echo pass
+
+curl -LGO https://raw.githubusercontent.com/artembayguzhin/files/main/dante.sh
+chmod 777 dante.sh
+sudo ./dante.sh
+sudo /etc/init.d/sockd adduser pass pass
+echo 'net.ipv4.icmp_echo_ignore_all = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
